2635f2a49b69fe72e9c55a3a28d1836a530679f2
Adriana Suarez <adrianasuarez@Adrianas-MBP.fritz.box>
2018-01-13 23:17:22 +0100

    chapter 2

--- chapter02/Prueba.java
+++ chapter02/Prueba.java
@@ -1,11 +1,25 @@
 public class Prueba
 {
-    public static void main(String[] args)
-    {
-        String greeting = "Welcome to Core Java!";
+    public static void main(String[] args) {
+       /* String greeting = "Welcome to Core Java!";
         System.out.println(greeting);
         for (int i = 0; i < greeting.length(); i++)
             System.out.print("=");
-        System.out.println();
+        System.out.println();*/
+        String hola = "";
+ /*       System.out.println(hola.codePointAt(0));
+        System.out.println(hola.codePointAt(1));
+        System.out.println(hola.codePointAt(2));
+        System.out.println(hola.codePointAt(3));
+*/
+        // System.out.println(System.getProperty("user.dir"));
+        label:
+        {
+            if(hola.isEmpty()) {
+                System.out.println("sale");
+                break label;
+            }
+            System.out.print("no sale");
+        }
     }
 }
